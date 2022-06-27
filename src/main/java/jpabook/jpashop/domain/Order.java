c7ad04d12be715b3cad01659d12af975656b6888
Sion Noh <shtldhs123@ajou.ac.kr>
2022-06-27 21:28:04 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -38,6 +38,10 @@ public class Order {
 
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
+    public void updateStatus(OrderStatus status) {
+        this.status = status;
+    }
+
     //== 연관관계 편의 메소드 ==//
     public void setMember(Member member) {
         this.member = member;
@@ -52,5 +56,45 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.updateOrder(this);
+    }
+
+    //== 생성 메소드 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = Order.builder()
+                .member(member)
+                .delivery(delivery)
+                .status(OrderStatus.ORDER)
+                .orderDate(LocalDateTime.now())
+                .build();
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        return order;
+    }
+
+    //== 비즈니스 로직 ==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+        this.updateStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //== 조회 로직 ==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -34,4 +34,29 @@ public class OrderItem {
     public void updateOrder(Order order) {
         this.order = order;
     }
+
+    //== 생성 메소드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = OrderItem.builder()
+                .item(item)
+                .orderPrice(orderPrice)
+                .count(count)
+                .build();
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //== 조회 로직 ==//
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
